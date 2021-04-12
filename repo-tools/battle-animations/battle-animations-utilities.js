7c0f11dee92487c40119ccacb0173eac9cf82221
Brady Shinners <brady.shinners@gmail.com>
2021-04-12 12:39:24 -0500

    feat(README): Add script to autogenerate battle anim dir readmes on commit

---
+++ repo-tools/battle-animations/battle-animations-utilities.js
@@ -0,0 +1,147 @@
+/**
+ * Utility for setting the appropriate heading level in reused markdown.
+ * NOTE: markdown syntax caps at depth of 6.
+ * 
+ * @param {number} [depth=1] 
+ * 
+ * @returns {string} # times depth (e.g.## or ######)
+ */
+const getHeading = (depth = 1) => '#'.repeat(depth)
+
+/**
+ * escapes parens in strings for safe markdown link
+ * @param {string} string 
+ * 
+ * @returns {string}
+ */
+const escapeParens = (string) => string.replace(/(?=[()\[\]])/g, '\\')
+
+/**
+ * Given anim and weapon objects, generates the README text for an anim weapon folder.
+ * 
+ * @param {Object} anim 
+ * @param {Object} weapon
+ * 
+ * @returns {String}
+ */
+const makeWeaponReadmeText = (anim, weapon) => (
+	`# [${escapeParens(anim.name)}](../)
+
+## Credit
+
+${anim.credits}
+	
+## ${weapon.type}
+
+| Still | Animation |
+| :---: | :-------: |
+| ![${weapon.type} still](./${encodeURI(weapon.static)}) | ![${weapon.type} animation](./${encodeURI(weapon.active)}) |
+`
+)
+
+/**
+ * Given anim and weapon objects, generates the README text for an anim weapon folder.
+ * 
+ * @param {Object} anim
+ * @param {boolean} [addCredits=true] determines whether the credits should be added to the README.
+ * @param {string} [currentDir='./'] the relative path that images should use
+ * @param {number} [headingDepth=1] the depth of the markdown headers
+ * 
+ * @returns {String}
+ */
+const makeAnimReadmeText = (anim, addCredits = true, currentDir = './', headingDepth = 1) => {
+	let weaponHeaders = '|'
+	let weaponContents = '|'
+	anim.weapons.forEach(weapon => {
+		let weaponDir = ''
+		if (weapon.dir) weaponDir = weapon.dir + '/'
+		weaponHeaders += `${weapon.type} |`
+		weaponContents += 
+	` <img alt="${weapon.type} animation" src="${encodeURI(currentDir + weaponDir + weapon.active)}" /> |`
+	})
+	// NOTE: if exact dimensions are desired, pass height="160" and width="248" to the <img tags in this file
+	// I am not currently doing this because I do not know what github will default to.
+
+	let creditsBlock = ''
+	if (addCredits) {
+		creditsBlock = `${getHeading(headingDepth + 1)} Credits
+
+${anim.credits}
+`
+	}
+
+	let showPreviewBlock, showWeaponsHeading
+	if (anim.weapons[0].dir) {
+		showPreviewBlock = true
+		showWeaponsHeading = true
+	}
+
+	return (
+		`${getHeading(headingDepth)} [${escapeParens(anim.name)}](${encodeURI(`${currentDir}${anim.name})`)}
+${showPreviewBlock ? `
+<img src="${encodeURI(currentDir +anim.weapons[0].dir + '/' + anim.weapons[0].static)}" alt="${anim.name} standing" />
+`: ''}
+${creditsBlock}
+${showWeaponsHeading ? `${getHeading(headingDepth +1)} Weapons
+` : ''}
+
+${weaponHeaders}
+| ${anim.weapons.map(() => ' :---: |').join('')}
+${weaponContents}
+`)
+}
+
+/**
+ * Given anim objects from a specific dir, generates the README text for a category folder.
+ * Intentionally does not include credits, but links to the specific anim dir that does.
+ * 
+ * @param {Object[]} catAnims
+ * @param {string} catDir the directory for all these animations
+ * @param {string} [currentDir='./'] the relative path that images should use
+ * @param {number} [headingDepth=1] the depth of the markdown headers
+ * 
+ * @returns {String}
+ */
+const makeCategoryReadmeText = (catAnims, catDir, currentDir='./', headingDepth = 1) => {
+
+	const animContents = catAnims.map(anim => {
+		return makeAnimReadmeText(anim, false, `${currentDir}${anim.name}/`, headingDepth + 1)
+	}).join(`
+
+`)
+
+	return (
+		`${getHeading(headingDepth)} [${catDir} Battle Animations](${encodeURI(`${currentDir}`)})
+
+${animContents}
+`)
+}
+
+/**
+ * Given anim objects from a specific dir, generates the README text for a category folder.
+ * Intentionally does not include credits, but links to the specific anim dir that does.
+ * 
+ * @param {Array[]} allAnims
+ * @param {string} [currentDir='./'] the relative path that images should use
+ * @param {number} [headingDepth=1] the depth of the markdown headers
+ * 
+ * @returns {String}
+ */
+const makeRootReadmeText = (allAnims, currentDir='./', headingDepth = 1) => {
+	const categorizedAnimContents = allAnims.map(({ anims, dir }) => makeCategoryReadmeText(anims, dir, `${currentDir}${dir}/`, headingDepth + 1)).join(`
+
+`)
+
+	return (
+		`${getHeading(headingDepth)} Battle Animations
+
+${categorizedAnimContents}
+`)
+}
+
+module.exports = {
+	makeAnimReadmeText,
+	makeWeaponReadmeText,
+	makeCategoryReadmeText,
+	makeRootReadmeText
+}
---
+++ repo-tools/battle-animations/make-battle-animations-readme.js
@@ -0,0 +1,115 @@
+const fs = require('fs').promises;
+const { logMissingFile } = require('../utilities')
+const { makeAnimReadmeText, makeWeaponReadmeText, makeRootReadmeText, makeCategoryReadmeText } = require('./battle-animations-utilities')
+
+const ROOT_DIR = './Battle Animations'
+const README_FILENAME = 'README.md';
+
+/**
+ * Parses the "Battle Animations" dirs and gathers all the animations in a flat list.
+ * 
+ * @returns {Object[]}
+ */
+const searchAnimations = async () => {
+	const battleAnimationsFiles = await fs.readdir(ROOT_DIR, { withFileTypes: true })
+	const categoryDirectories = battleAnimationsFiles.reduce((accumulator, categoryFile) => {
+		if (categoryFile.isDirectory()) accumulator.push(categoryFile.name)
+		return accumulator
+	}, [])
+	Promise.all(categoryDirectories.map(async categoryDir => {
+		const categoryDirectoryFiles = await fs.readdir(`${ROOT_DIR}/${categoryDir}`, { withFileTypes: true })
+		const animDirectories = categoryDirectoryFiles.reduce((accumulator, categoryFile) => {
+			if (categoryFile.isDirectory()) accumulator.push(categoryFile.name)
+			return accumulator
+		}, [])
+
+		return Promise.all(await animDirectories.map(async animDir => {
+			const anim = {
+				name: animDir,
+				// this fallback can be changed to '' when the credits work is complete
+				credits: "Credits are currently in the process of being implemented fully. Please use the names within \{\} in the title until the work is complete."
+			}
+			try {
+				const credits = await fs.readFile(`${ROOT_DIR}/${categoryDir}/${animDir}/credits.txt`)
+				anim.credits = credits
+			} catch {
+				console.log(`the file: ${ROOT_DIR}/${categoryDir}/${animDir}/credits.txt does not exist`)
+			}
+			// Remove this 'if' if Spells removed from Battle Animations dir
+			if (categoryDir === '7. Spells') {
+				const type = 'Spell'
+				const static = `${type}_b_001.png`
+				const active = `${type}.gif`
+				
+				const weapon = {
+					type,
+					static,
+					active
+				}
+				logMissingFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${static}`)
+				logMissingFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${active}`)
+				anim.weapons = [weapon]
+				fs.writeFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${README_FILENAME}`, makeWeaponReadmeText(anim, weapon))
+				return anim
+			}
+			// Remove this 'if' if Skills removed from Battle Animations dir
+			else if (categoryDir === '8. Skills') {
+				const type = 'Skill'
+				const static = `${type}_g000.png`
+				const active = `${type}.gif`
+
+				const weapon = {
+					type,
+					static,
+					active
+				}
+				logMissingFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${static}`)
+				logMissingFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${active}`)
+				anim.weapons = [weapon]
+				fs.writeFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${README_FILENAME}`, makeWeaponReadmeText(anim, weapon))
+				return anim
+			}
+			// search unit anims w/ weapon dirs
+
+			const animContents = await fs.readdir(`${ROOT_DIR}/${categoryDir}/${animDir}`, { withFileTypes: true })
+			const weaponDirs = animContents.reduce((accumulator, current) => {
+				if (current.isDirectory()) accumulator.push(current.name)
+				return accumulator
+			}, [])
+			
+			return Promise.all(weaponDirs.map(weaponDir => {
+				const type = weaponDir.replace(/\d*?\.\s/, "").split(" ")[0]
+				const static = `${type}_000.png`
+				const active = `${type}.gif`
+				const weapon = {
+					active,
+					dir: weaponDir,
+					static,
+					type,
+				}
+				logMissingFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${weaponDir}/${static}`)
+				logMissingFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${weaponDir}/${active}`)
+				
+				fs.writeFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${weaponDir}/${README_FILENAME}`, makeWeaponReadmeText(anim, weapon))
+
+				return weapon
+			})).then(weapons => {
+				anim.weapons = weapons
+	
+				fs.writeFile(`${ROOT_DIR}/${categoryDir}/${animDir}/${README_FILENAME}`, makeAnimReadmeText(anim))
+	
+				return anim
+			})
+		})).then(categoryAnims => {
+	
+			fs.writeFile(`${ROOT_DIR}/${categoryDir}/${README_FILENAME}`, makeCategoryReadmeText(categoryAnims, categoryDir))
+			return { anims: categoryAnims, dir: categoryDir }
+		})
+	})).then(anims => {
+		fs.writeFile(`${ROOT_DIR}/${README_FILENAME}`, makeRootReadmeText(anims))
+		return anims
+	})
+
+}
+
+searchAnimations()
